23a9483b5b4db13157a421193dc110edad6f4ef5
createweb7 <createweb7@gmail.com>
2026-07-06 19:35:40 +0530

    Switch homepage from force-dynamic to revalidate=300 for faster social previews

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic';
+export const revalidate = 300;
 
 import Hero from '../../components/sections/Hero';
 import Clients from '../../components/sections/Clients';
